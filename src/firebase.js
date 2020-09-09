8e0340c5b313d12aed90403cc4e842084cd4ea7c
DhanushM12 <dhanushmukhi@gmail.com>
2020-09-10 04:37:01 +0530

    basic folder structure created and firebase config added

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC-tDFpjxWFlCgKO9-w-O_f5TS2GBOyNIA",
+  authDomain: "challenge-9f6b8.firebaseapp.com",
+  databaseURL: "https://challenge-9f6b8.firebaseio.com",
+  projectId: "challenge-9f6b8",
+  storageBucket: "challenge-9f6b8.appspot.com",
+  messagingSenderId: "780988405537",
+  appId: "1:780988405537:web:b2e5fa0cdfb74e2f521060",
+};
